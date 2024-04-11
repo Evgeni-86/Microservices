29a752f14e2723900b94670688e1cfddb1c95d03
batanov evgeni <batanovevgeni@mail.ru>
2024-04-11 18:49:22 +0300

    add evreka secyrity

---
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+//@EnableWebFluxSecurity
+//@Configuration
+//public class SecurityConfiguration {
+//
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity.csrf((csrf) -> csrf.disable())
+//                .authorizeExchange((exchanges) -> exchanges
+//                        .pathMatchers("/eureka/**")
+//                        .permitAll()
+//                        .anyExchange()
+//                        .authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+//        return serverHttpSecurity.build();
+//    }
+//}
---
+++ discovery-server/src/main/java/com/example/discoveryserver/config/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.example.discoveryserver.config;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Value("${eureka.username}")
+    private String userName;
+    @Value("${eureka.password}")
+    private String userPass;
+
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername(userName)
+                .password(bCryptPasswordEncoder.encode(userPass))
+                .roles("USER")
+                .build());
+        return manager;
+    }
+
+    @Bean BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated())
+                .httpBasic((Customizer.withDefaults()));
+        return http.build();
+    }
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> web.ignoring().requestMatchers("/css/**");
+//    }
+}
